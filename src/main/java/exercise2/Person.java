10ac7e0bd253127fa60404b811351048e7fb026b
Fa <farid-f32@msn.com>
2018-11-02 15:02:25 +0100

    Résolu exercice  retourne tout les voyants vert

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,25 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int popsize=0;
+	private static int sumage=0;
+	public Person (int age)
+	{
+		this.age=age;
+		popsize++;
+		sumage +=age;
+	}
+	static int computePopulationSize() {
+
+		return popsize;
+	}
+	static float computeAveragePopulationAge() {
+
+		return (float)sumage/popsize;
+	}
+	static void resetPopulation() {
+		popsize=0;
+		sumage=0;
+	}
 }
